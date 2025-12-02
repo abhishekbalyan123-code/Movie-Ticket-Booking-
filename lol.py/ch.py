9169ba6e73975896313ab30953c6bdcf055d9a7f
abhishekbalyan123-code <abhishekbalyan123@gmail.com>
2025-12-02 21:58:45 +0530

    Update ch.py

--- lol.py/ch.py
+++ lol.py/ch.py
@@ -1,9 +1,1 @@
-int(input('source of kids:'))
-print()
-source_of_kids = 34
-if source_of_kids > 40:
-    print("pedo")
-elif source_of_kids < 10:
-    print("shut the school")
-else:
-    print("nothing")
+
